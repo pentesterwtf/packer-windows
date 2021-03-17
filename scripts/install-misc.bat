eb74229b8e6076228df0de2b858b25697d3f6b8f
SomeSysadmin <github@c0refailure.com>
2021-03-18 09:24:41 +1100

    Qemu improvements (#4)
    * Add Virtio Serial driver
    
    * Add baloon and viorng drivers
    
    * Add old redhat cert, install spice-agent with chocolatey
    
    * Remove adding dotnetfx, breaking server builds?
    
    Co-authored-by: mayhem <mayhem@c0refailure.com>

--- scripts/install-misc.bat
+++ scripts/install-misc.bat
@@ -3,13 +3,13 @@ REM choco should work here
 
 msiexec /qb /i "https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-qemu-ga/qemu-ga-win-100.0.0.0-3.el7ev/qemu-ga-x64.msi"
 
+REM Install spice-agent
+choco install spice-agent -y
+
 REM Copy our sysprep Autounattend for our post-packer first boot
 copy "A:/Firstboot-Autounattend.xml" "C:/Windows/Temp/Autounattend.xml"
 REM Copy the enable-winrm script, relied on by our post-packer autounattend script
 copy "A:/enable-winrm.ps1" "C:/Windows/Temp/enable-winrm.ps1"
-
-REM Install dotnetfx
-choco install dotnetfx -y
 
 REM Set high performance mode
 powercfg /SETACTIVE 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
