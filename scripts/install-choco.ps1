e5605e266c4b85e6ddc54a6fff3e0e98383bef65
Mayhem <mayhem@c0refailure.com>
2021-08-13 17:04:50 +1000

    Bugfix - install choco support for older OS's

--- scripts/install-choco.ps1
+++ scripts/install-choco.ps1
@@ -7,7 +7,8 @@ certutil -addstore -f "TrustedPublisher" a:/redhat-cert.cer
 certutil -addstore -f "TrustedPublisher" a:/redhat-cert-old.cer
 
 # Install chocolatey
-cmd /c C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
+# Now with TLS workarounds for Server 2016 - https://docs.chocolatey.org/en-us/choco/setup and https://github.com/TheJumpCloud/support/issues/221
+cmd /c C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
 
 # Due to PATH, choco probably won't work here...
 # Also install openssh here
