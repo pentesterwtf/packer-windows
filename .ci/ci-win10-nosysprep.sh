deac311256190d1f48eb26927c011658a60b9c0f
mayhem <mayhem@c0refailure.com>
2022-01-26 11:21:28 +1100

    Add local CI bits

---
+++ .ci/ci-win10-nosysprep.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Used for internal CI
+# Probably not much value for others, but YMMV :)
+
+set -eux
+
+# Remove existing artefacts
+rm -rf packer_cache output-qemu builds
+
+packer build -var="vm_name=windows_10" -var="shutdown_command=shutdown /s /t 10 /f /d p:4:1 /c \"Packer Shutdown\"" -var="iso_checksum=8a38844da7bdbe77de7d8227ac5962fff357abcc" -var="iso_url=/opt/iso/19042.631.201119-0144.20h2_release_svc_refresh_CLIENTENTERPRISEEVAL_OEMRET_x64FRE_en-us.iso" packer.json
+
+# Push to S3
+mc cp output-qemu/packer-qemu s3/pentesterwtf/qemu/windows-10-1904-nosysprep.qcow2
---
+++ .ci/ci-win2019-nosysprep.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Used for internal CI
+# Probably not much value for others, but YMMV :)
+
+set -eux
+
+# Remove existing artefacts
+rm -rf packer_cache output-qemu builds
+
+packer build -var="vm_name=windows_server_2019" -var="shutdown_command=shutdown /s /t 10 /f /d p:4:1 /c \"Packer Shutdown\"" -var "autounattend=./answer_files/2019/Autounattend.xml" -var="iso_checksum=91e3a2f1acc39af21353c7cc105c799494d7539f" -var="iso_url=/opt/iso/17763.253.190108-0006.rs5_release_svc_refresh_SERVER_EVAL_x64FRE_en-us.iso"  packer.json
+
+# Push to S3
+mc cp output-qemu/packer-qemu s3/pentesterwtf/qemu/windows-server-2019-17763-nosysprep.qcow2
